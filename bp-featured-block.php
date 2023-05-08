96c308795749218f0d7b8e6374be49ce9622869e
mdshofiullah <mdshofiullah65@gmail.com>
2023-05-09 02:38:59 +0600

    feature tab modified shows specific posts to specific group

--- bp-featured-block.php
+++ bp-featured-block.php
@@ -3,10 +3,8 @@
     <div class="mainActiveFeedBlock white-pad">
         <div class="groupFeedList">
 
-
             <div class="displayGroupFeedList">
                 <div class="">
-
                     <?php
                     $bp_ids = bp_get_pin_posts_ids();
                     // var_dump($bp_ids);
@@ -16,15 +14,12 @@
 //var_dump($ids_array);
                         foreach ($ids_array as $activity_id)
                         {
-
                             $activity = bp_activity_get_specific( array(
                                 'activity_ids' => $activity_id,
                             ) );
                             $activity = $activity['activities'][0];
 
-
                             $activity_meta = bp_activity_get_meta($activity_id);
-
                             $user_id = $activity->user_id;
                             $user = get_userdata( $user_id );
                             $full_name = $user->first_name . ' ' . $user->last_name;
@@ -32,10 +27,7 @@
                             $username = bp_core_get_username( $user_id );
                             $post_time = $activity->date_recorded;
                             $formatted_post_time = date( 'F j, Y, g:i a', strtotime( $post_time ) );
-
                             $time_diff = human_time_diff( strtotime( $post_time ), current_time( 'timestamp' ) );
-
-
 
                             if(array_key_exists("bp_media_ids",$activity_meta))
                             {
@@ -61,12 +53,9 @@
                                 $bp_video_url = '';
                             }
 
-
                             $activitycontent    = $activity->content;
 
-
                             ?>
-
 
                             <div class="c-singleFeedWrap">
                                 <div class="c-feedUserInfo">
@@ -94,19 +83,13 @@
                                         </video>
                                     <?php } ?>
 
-
                                 </div>
                             </div>
-
 
                         <?php }
 
                     endif;
                     ?>
-
-
-
-
 
                 </div>
             </div>
@@ -120,7 +103,7 @@
     <div class="groupMainRight">
         <div class="singleWidgetBlock widgetsList white-pad">
             <div class="c-secTitle">
-                <h3>ABOUT1</h3>
+                <h3>ABOUT</h3>
             </div>
             <p>
                 This FBB Community Group will be the hub of all the activities in relation to your FBB course. Here, the Bamboo U Team will post any important information, announcements and achievements which relate to the course. Students are encouraged to post their questions (#ASK), queries (#TeamBambooU) and wins (#WIN) on this group’s page.
--- updated-bp-featured-block.php
+++ updated-bp-featured-block.php
@@ -1,112 +1,106 @@
 <div class="featured_block">
-
     <div class="mainActiveFeedBlock white-pad">
         <div class="groupFeedList">
-
-
             <div class="displayGroupFeedList">
                 <div class="">
-
                     <?php
                     $bp_ids = bp_get_pin_posts_ids();
                     // var_dump($bp_ids);
                     $ids = $bp_ids;
+                    //	Modified By LS Start
+                    $group_id = bp_get_current_group_id();
+                    //	Modified By LS End
                     if (!empty($ids)):
                         $ids_array = $ids;
 //var_dump($ids_array);
-                        foreach ($ids_array as $activity_id)
-                        {
+// foreach ($ids_array as $activity_id)
 
+// Modified By Ls Start
+                        foreach ($ids_array as $activity_id)
+// Modified By LS End
+                        {
                             $activity = bp_activity_get_specific( array(
+// 			'activity_ids' => $activity_id,
+                                // Modified By Ls Start
                                 'activity_ids' => $activity_id,
+// Modified By LS End
                             ) );
                             $activity = $activity['activities'][0];
+//     Modified By Ls Start
+                            // Only show posts that belong to this group and have component type 'groups'
+                            if ($activity->item_id == $group_id && $activity->component == 'groups') {
+//     Modified By LS End
+                                $activity_meta = bp_activity_get_meta($activity_id);
+                                $user_id = $activity->user_id;
+                                $user = get_userdata( $user_id );
+                                $full_name = $user->first_name . ' ' . $user->last_name;
+                                $avatar = bp_core_fetch_avatar( array( 'item_id' => $user_id ) );
+                                $username = bp_core_get_username( $user_id );
+                                $post_time = $activity->date_recorded;
+                                $formatted_post_time = date( 'F j, Y, g:i a', strtotime( $post_time ) );
+                                $time_diff = human_time_diff( strtotime( $post_time ), current_time( 'timestamp' ) );
 
+                                if(array_key_exists("bp_media_ids",$activity_meta))
+                                {
+                                    $bp_photoids = $activity_meta['bp_media_ids'][0];  // get 1st photo id
+                                    $bp_getimage = bp_media_get_specific (array ('media_ids' => $bp_photoids) );
+                                    $photourl = $bp_getimage["medias"][0]->attachment_data->thumb;
+                                }else{
+                                    $photourl = '';
+                                }
 
-                            $activity_meta = bp_activity_get_meta($activity_id);
+                                if(array_key_exists("bp_video_ids",$activity_meta)){
 
-                            $user_id = $activity->user_id;
-                            $user = get_userdata( $user_id );
-                            $full_name = $user->first_name . ' ' . $user->last_name;
-                            $avatar = bp_core_fetch_avatar( array( 'item_id' => $user_id ) );
-                            $username = bp_core_get_username( $user_id );
-                            $post_time = $activity->date_recorded;
-                            $formatted_post_time = date( 'F j, Y, g:i a', strtotime( $post_time ) );
+                                    $bp_video_ids = $activity_meta['bp_video_ids'][0]; // this is video id
+                                    $video_attachment_id_array = BP_Media::get( $args = array(
+                                        'in'=> $bp_video_ids,
+                                        'video'=> true,
+                                    ) );
+                                    $video_attachment_id = $video_attachment_id_array["medias"][0]->attachment_id;
+                                    $bp_video_url = wp_get_attachment_url($video_attachment_id);
 
-                            $time_diff = human_time_diff( strtotime( $post_time ), current_time( 'timestamp' ) );
+                                }else{
+                                    $bp_video_url = '';
+                                }
 
+                                $activitycontent    = $activity->content;
 
-
-                            if(array_key_exists("bp_media_ids",$activity_meta))
-                            {
-                                $bp_photoids = $activity_meta['bp_media_ids'][0];  // get 1st photo id
-                                $bp_getimage = bp_media_get_specific (array ('media_ids' => $bp_photoids) );
-                                $photourl = $bp_getimage["medias"][0]->attachment_data->thumb;
-                            }else{
-                                $photourl = '';
-                            }
-
-
-                            if(array_key_exists("bp_video_ids",$activity_meta)){
-
-                                $bp_video_ids = $activity_meta['bp_video_ids'][0]; // this is video id
-                                $video_attachment_id_array = BP_Media::get( $args = array(
-                                    'in'=> $bp_video_ids,
-                                    'video'=> true,
-                                ) );
-                                $video_attachment_id = $video_attachment_id_array["medias"][0]->attachment_id;
-                                $bp_video_url = wp_get_attachment_url($video_attachment_id);
-
-                            }else{
-                                $bp_video_url = '';
-                            }
-
-
-                            $activitycontent    = $activity->content;
-
-
-                            ?>
-
-
-                            <div class="c-singleFeedWrap">
-                                <div class="c-feedUserInfo">
-                                    <div class="c-userpic">
-                                        <?=$avatar;?>
+                                ?>
+                                <div class="c-singleFeedWrap">
+                                    <div class="c-feedUserInfo">
+                                        <div class="c-userpic">
+                                            <?=$avatar;?>
+                                        </div>
+                                        <div class="c-usernickname"><h4><?=$full_name;?> <span>@<?=$username;?> . <?=$time_diff;?> ago</span></h4></div>
                                     </div>
-                                    <div class="c-usernickname"><h4><?=$full_name;?> <span>@<?=$username;?> . <?=$time_diff;?> ago</span></h4></div>
+                                    <div class="displayFeedPost">
+
+                                        <?php
+                                        if($photourl != null){
+                                            ?>
+                                            <img src="<?=$photourl;?>" alt="" title="">
+                                        <?php } ?>
+
+                                        <?=$activitycontent;?>
+
+                                        <?php
+                                        if($bp_video_url != null){
+                                            ?>
+                                            <video width="100%" height="99%" controls="controls" type="video/mp4" preload="none">
+                                                <source src="<?=$bp_video_url;?>" autostart="false">
+                                                Your browser does not support the video tag.
+                                            </video>
+                                        <?php } ?>
+
+                                    </div>
                                 </div>
-                                <div class="displayFeedPost">
 
-                                    <?php
-                                    if($photourl != null){
-                                        ?>
-                                        <img src="<?=$photourl;?>" alt="" title="">
-                                    <?php } ?>
-
-                                    <?=$activitycontent;?>
-
-                                    <?php
-                                    if($bp_video_url != null){
-                                        ?>
-                                        <video width="100%" height="99%" controls="controls" type="video/mp4" preload="none">
-                                            <source src="<?=$bp_video_url;?>" autostart="false">
-                                            Your browser does not support the video tag.
-                                        </video>
-                                    <?php } ?>
-
-
-                                </div>
-                            </div>
-
-
-                        <?php }
-
+                            <?php }
+// 	 Modified BY LS Start
+                        }
+// 	Modified By LS END
                     endif;
                     ?>
-
-
-
-
 
                 </div>
             </div>
@@ -114,13 +108,10 @@
         </div>
     </div>
 
-
-
-
     <div class="groupMainRight">
         <div class="singleWidgetBlock widgetsList white-pad">
             <div class="c-secTitle">
-                <h3>ABOUT1</h3>
+                <h3>ABOUT</h3>
             </div>
             <p>
                 This FBB Community Group will be the hub of all the activities in relation to your FBB course. Here, the Bamboo U Team will post any important information, announcements and achievements which relate to the course. Students are encouraged to post their questions (#ASK), queries (#TeamBambooU) and wins (#WIN) on this group’s page.
@@ -128,7 +119,6 @@
                 This group will become a powerful tool throughout the course’s duration and then beyond! So make sure to engage and participate here so that you can start to build your Bamboo Community.
             </p>
         </div>
-
 
         <div class="singleWidgetBlock">
             <div class="imgWidget">
@@ -138,8 +128,6 @@
                 </a>
             </div>
         </div>
-
-
 
     </div>
 
